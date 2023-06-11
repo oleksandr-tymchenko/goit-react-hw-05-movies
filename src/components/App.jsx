e895029ac71b2da118e37bd2e610c30f170472ac
Oleksandr Tymchenko <116674828+oleksandr-tymchenko@users.noreply.github.com>
2023-06-11 19:52:13 +0300

    fixed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,66 +1,66 @@
-import { useEffect, useState } from 'react';
+// import { useEffect, useState } from 'react';
 
-import Modal from 'components/Modal/Modal';
+// import Modal from 'components/Modal/Modal';
 
-import SearchBar from './SearchBar/SearchBar';
+// import SearchBar from './SearchBar/SearchBar';
 import { Container } from './App.styled';
-import { Btn } from './Button/Button.styled';
-import { Loader } from './Loader/Loader';
+// import { Btn } from './Button/Button.styled';
+// import { Loader } from './Loader/Loader';
 
-import { getImages } from 'servises/api';
+// import { getImages } from 'servises/api';
 
-import { ErrorMessage } from './ErrorMessage/ErrorMessage';
-import { ImageGalery } from './ImageGallery/ImageGallery';
+// import { ErrorMessage } from './ErrorMessage/ErrorMessage';
+// import { ImageGalery } from './ImageGallery/ImageGallery';
 
 export const App = () => {
-  const [images, setImages] = useState([]);
-  const [largeImgURL, setLargeImgURL] = useState('');
-  const [value, setValue] = useState('');
-  const [page, setPage] = useState(1);
-  // const [per_page] = useState(12);
-  const [isError, setIsError] = useState('');
-  const [showBtn, setShowBtn] = useState(false);
-  const [isEmpty, setIsEmpty] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
+  // const [images, setImages] = useState([]);
+  // const [largeImgURL, setLargeImgURL] = useState('');
+  // const [value, setValue] = useState('');
+  // const [page, setPage] = useState(1);
+  // // const [per_page] = useState(12);
+  // const [isError, setIsError] = useState('');
+  // const [showBtn, setShowBtn] = useState(false);
+  // const [isEmpty, setIsEmpty] = useState(false);
+  // const [isLoading, setIsLoading] = useState(false);
 
-  useEffect(() => {
-    if (!value) {
-      return;
-    }
-    setIsLoading(true);
-    getImages(value, page)
-      .then(data => {
-        setIsEmpty(!data.hits.length);
-        setImages(i => [...i, ...data.hits]);
-        setShowBtn(page < Math.ceil(data.total / 12));
-      })
-      .catch(error => {
-        setIsError(true);
-      })
-      .finally(() => {
-        setIsLoading(false);
-      });
-  }, [page, value]);
+  // useEffect(() => {
+  //   if (!value) {
+  //     return;
+  //   }
+  //   setIsLoading(true);
+  //   getImages(value, page)
+  //     .then(data => {
+  //       setIsEmpty(!data.hits.length);
+  //       setImages(i => [...i, ...data.hits]);
+  //       setShowBtn(page < Math.ceil(data.total / 12));
+  //     })
+  //     .catch(error => {
+  //       setIsError(true);
+  //     })
+  //     .finally(() => {
+  //       setIsLoading(false);
+  //     });
+  // }, [page, value]);
 
-  const nextPage = () => {
-    setPage(page => page + 1);
-  };
+  // const nextPage = () => {
+  //   setPage(page => page + 1);
+  // };
 
-  const handleSubmit = value => {
-    setValue(value);
-    setPage(1);
-    setImages([]);
-    setShowBtn(false);
-    setIsEmpty(false);
-  };
+  // const handleSubmit = value => {
+  //   setValue(value);
+  //   setPage(1);
+  //   setImages([]);
+  //   setShowBtn(false);
+  //   setIsEmpty(false);
+  // };
 
-  const toggleModal = link => {
-    setLargeImgURL(link);
-  };
+  // const toggleModal = link => {
+  //   setLargeImgURL(link);
+  // };
 
   return (
     <Container>
-      <SearchBar onSubmit={handleSubmit} />
+      {/* <SearchBar onSubmit={handleSubmit} />
       <ImageGalery images={images} openModal={toggleModal} />
       {isEmpty && (
         <ErrorMessage>
@@ -74,7 +74,7 @@ export const App = () => {
         <Modal onClose={toggleModal}>
           <img src={largeImgURL} alt="largeImg" />
         </Modal>
-      )}
+      )} */}
     </Container>
   );
 };
