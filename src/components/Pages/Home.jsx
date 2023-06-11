c76bf755f89ef1846b0e4e124e3cb31349b7d77d
Oleksandr Tymchenko <116674828+oleksandr-tymchenko@users.noreply.github.com>
2023-06-11 23:53:04 +0300

    add links to home

--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -1,6 +1,7 @@
 import { Container } from 'components/App.styled';
 import { useEffect, useState } from 'react';
 import React from 'react';
+import { Link } from 'react-router-dom';
 import getMovies from 'servises/api';
 console.log(getMovies);
 const Home = () => {
@@ -32,7 +33,11 @@ const Home = () => {
       <h1> The best daily movies for you</h1>
       <ul>
         {movies.map(({ id, title, name }) => {
-          return <li key={id}>{name || title}</li>;
+          return (
+            <li key={id}>
+              <Link to={name || title}>{name || title}</Link>
+            </li>
+          );
         })}
       </ul>
     </Container>
